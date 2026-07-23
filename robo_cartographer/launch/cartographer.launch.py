5a4c4775234373db451d7feb019cb83dcafbe626
Talaron18 <mainauctor@gmail.com>
2026-07-23 22:47:18 +0800

    new file:   robo_cartographer/config/pr2.lua 	new file:   robo_cartographer/config/revo_lds.lua 	new file:   robo_cartographer/config/taurob_tracker.lua 	new file:   robo_cartographer/config/transform.lua 	new file:   robo_cartographer/config/visualize_pbstream.lua 	new file:   robo_cartographer/launch/cartographer.launch.py 	new file:   yesense_ros2/yesense_ros2/README 	new file:   yesense_ros2/yesense_ros2/build.sh 	new file:   yesense_ros2/yesense_ros2/clean.sh 	new file:   yesense_ros2/yesense_ros2/gen_interface.sh

---
+++ robo_cartographer/launch/cartographer.launch.py
@@ -0,0 +1,68 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+
+    pkg_path = get_package_share_directory("robo_cartographer")
+
+    config_dir = os.path.join(pkg_path, "config")
+
+    return LaunchDescription([
+
+        Node(
+            package="cartographer_ros",
+            executable="cartographer_node",
+            output="screen",
+
+            parameters=[
+                {"use_sim_time": False}
+            ],
+
+            arguments=[
+                "-configuration_directory", config_dir,
+                "-configuration_basename", "my_car.lua"
+            ],
+
+            remappings=[
+                ("scan", "/scan"),
+                ("imu", "/imu/data_raw"),
+            ]
+        ),
+
+        Node(
+            package="cartographer_ros",
+            executable="cartographer_occupancy_grid_node",
+            output="screen",
+
+            arguments=[
+                "-resolution", "0.05"
+            ]
+        ),
+
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            name="laser_tf",
+            arguments=[
+                "0", "0", "0",
+                "0", "0", "0",
+                "base_link",
+                "laser",
+            ],
+        ),
+
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            name="imu_tf",
+            arguments=[
+                "0", "0", "0",
+                "0", "0", "0",
+                "base_link",
+                "gyro_link",
+            ],
+        )
+    ])
